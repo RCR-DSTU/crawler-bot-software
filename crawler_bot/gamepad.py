c6d710aa4b980e265b3713e942dea1a24de00322
Maisonig <Israelyan-harry@mail.ru>
2023-09-21 09:17:20 +0300

    The original version of the project. The repository was created after 2 weeks of development.

---
+++ crawler_bot/gamepad.py
@@ -0,0 +1,172 @@
+from threading import Thread
+from pyPS4Controller.controller import Controller
+
+from crawler_bot import config
+
+
+class Gamepad(Controller):
+    """
+    Инициализация экземпляра класса контроллера (геймпада) Playstation 4 (Dualshock 4) для управления роботом в
+    ручном режиме. Работа слушателя осуществляется в параллельном потоке все время существования экземпляра после
+    инициализации.
+    """
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+        self.Logger = config.common_logger
+        self.Logger.info(" Gamepad initialization started.")
+
+        self.linear_velocity = 0.
+        self.angular_velocity = 0.
+
+        self.ratio = 0.000030488
+
+        self.threadListener = Thread(target=self.start_listening)
+        self.threadListener.start()
+
+    def on_x_press(self):
+        pass
+
+    def on_x_release(self):
+        pass
+
+    def on_triangle_press(self):
+        pass
+
+    def on_triangle_release(self):
+        pass
+
+    def on_circle_press(self):
+        pass
+
+    def on_circle_release(self):
+        pass
+
+    def on_square_press(self):
+        pass
+
+    def on_square_release(self):
+        pass
+
+    def on_L1_press(self):
+        pass
+
+    def on_L1_release(self):
+        pass
+
+    def on_L2_press(self, value):
+        pass
+
+    def on_L2_release(self):
+        pass
+
+    def on_R1_press(self):
+        pass
+
+    def on_R1_release(self):
+        pass
+
+    def on_R2_press(self, value):
+        pass
+
+    def on_R2_release(self):
+        pass
+
+    def on_up_arrow_press(self):
+        pass
+
+    def on_up_down_arrow_release(self):
+        pass
+
+    def on_down_arrow_press(self):
+        pass
+
+    def on_left_arrow_press(self):
+        pass
+
+    def on_left_right_arrow_release(self):
+        pass
+
+    def on_right_arrow_press(self):
+        pass
+
+    def on_R3_up(self, value):
+        # Значения меняются от 0 до -32800
+        self.linear_velocity = (- value) * self.ratio
+
+    def on_R3_down(self, value):
+        # Значения меняются от 0 до 32800
+        self.linear_velocity = (- value) * self.ratio
+
+    def on_R3_left(self, value):
+        # Значения меняются от 0 до -32800
+        self.angular_velocity = value * self.ratio
+
+    def on_R3_right(self, value):
+        # Значения меняются от 0 до 32800
+        self.angular_velocity = value * self.ratio
+
+    def on_R3_y_at_rest(self):
+        self.linear_velocity = 0.
+
+    def on_R3_x_at_rest(self):
+        self.angular_velocity = 0.
+
+    def on_L3_up(self, value):
+        pass
+
+    def on_L3_down(self, value):
+        pass
+
+    def on_L3_left(self, value):
+        pass
+
+    def on_L3_right(self, value):
+        pass
+
+    def on_L3_y_at_rest(self):
+        pass
+
+    def on_L3_x_at_rest(self):
+        pass
+
+    def on_L3_press(self):
+        pass
+
+    def on_L3_release(self):
+        pass
+
+    def on_R3_press(self):
+        pass
+
+    def on_R3_release(self):
+        pass
+
+    def on_options_press(self):
+        pass
+
+    def on_options_release(self):
+        pass
+
+    def on_share_press(self):
+        pass
+
+    def on_share_release(self):
+        pass
+
+    def on_playstation_button_press(self):
+        pass
+
+    def on_playstation_button_release(self):
+        pass
+
+    def connection_callback(self):
+        self.Logger.info(f" Gamepad connection established"
+                         f" \tInterface: {config.gamepad_interface}")
+
+    def disconnection_callback(self):
+        self.Logger.info(f" Gamepad connection lost!"
+                         f" \tInterface: {config.gamepad_interface}")
+
+    def start_listening(self):
+        self.listen(timeout=60)
---
+++ crawler_bot/paint.py
@@ -0,0 +1,58 @@
+import cv2
+import realsense
+import numpy as np
+
+from crawler_bot import config
+
+
+class Painter(object):
+
+    MINIMAP_SCALE = 0.3
+
+    WHITE_COLOR = (255, 255, 255)
+    GRAY_COLOR = (128, 128, 128)
+    BLACK_COLOR = (0, 0, 0)
+
+    def __init__(self):
+        self.Logger = config.common_logger
+
+        self.painterImage = None
+        self.minimapSize = (10, 10)
+        self.minimapRectangle = [0, 0, 0, 0]
+
+    def draw_minimap(self, dx, dy):
+        null_image = np.zeros(self.painterImage.shape, np.uint8)
+        null_image = cv2.rectangle(null_image,
+                                   self.minimapRectangle[:2],
+                                   self.minimapRectangle[2:4],
+                                   self.GRAY_COLOR,
+                                   -1,
+                                   )
+        self.painterImage = cv2.addWeighted(self.painterImage,
+                                            0.9,
+                                            null_image,
+                                            1.,
+                                            0.3)
+
+        return self.painterImage
+
+    def refresh_image(self, image):
+        image = np.array(image, np.uint8)
+        self.painterImage = image
+
+        mw = int(image.shape[1] * self.MINIMAP_SCALE)
+        mh = int(image.shape[0] * self.MINIMAP_SCALE)
+        self.minimapSize = (mw, mh)
+        self.minimapRectangle = [10, image.shape[0] - mh - 10, 10 + mw, image.shape[0] - 10]
+
+
+camera = realsense.IntelRealSenseCameraD455()
+camera.read_frames()
+color_frame = camera.get_color_frame()
+
+painter = Painter()
+painter.refresh_image(color_frame)
+painterImage = painter.draw_minimap(0, 0)
+
+cv2.imshow("Image", painterImage)
+cv2.waitKey(0)
